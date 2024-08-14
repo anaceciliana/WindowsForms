15f76732ef2bf599cf484da6f862183203252132
CJ3021963 <CJ3021963@LAB03-PC04>
2024-08-14 12:52:38 -0300

    Adicionar arquivos de projeto.

---
+++ WindowsForms/Form1.Designer.cs
@@ -0,0 +1,239 @@
+﻿namespace WindowsForms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txbName = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txbCpf = new System.Windows.Forms.TextBox();
+            this.txbTel = new System.Windows.Forms.TextBox();
+            this.txbEmail = new System.Windows.Forms.TextBox();
+            this.txbRg = new System.Windows.Forms.TextBox();
+            this.txbEnd = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.Info;
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Location = new System.Drawing.Point(-481, -273);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1918, 1087);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.Info;
+            this.label1.Font = new System.Drawing.Font("Linux Libertine G", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(184, 239);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(137, 18);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "INSIRA SEU NOME";
+            // 
+            // txbName
+            // 
+            this.txbName.BackColor = System.Drawing.Color.Cornsilk;
+            this.txbName.ForeColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.txbName.Location = new System.Drawing.Point(187, 276);
+            this.txbName.Name = "txbName";
+            this.txbName.Size = new System.Drawing.Size(134, 20);
+            this.txbName.TabIndex = 2;
+            this.txbName.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.Info;
+            this.label2.Font = new System.Drawing.Font("Linux Libertine G", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(425, 239);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(123, 18);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "DIGITE SEU CPF";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.SystemColors.Info;
+            this.label3.Font = new System.Drawing.Font("Linux Libertine G", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(624, 239);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(171, 18);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "DIGITE SEU TELEFONE";
+            // 
+            // txbCpf
+            // 
+            this.txbCpf.BackColor = System.Drawing.SystemColors.Info;
+            this.txbCpf.Location = new System.Drawing.Point(417, 276);
+            this.txbCpf.Name = "txbCpf";
+            this.txbCpf.Size = new System.Drawing.Size(140, 20);
+            this.txbCpf.TabIndex = 5;
+            this.txbCpf.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // txbTel
+            // 
+            this.txbTel.BackColor = System.Drawing.Color.Cornsilk;
+            this.txbTel.Location = new System.Drawing.Point(641, 276);
+            this.txbTel.Name = "txbTel";
+            this.txbTel.Size = new System.Drawing.Size(138, 20);
+            this.txbTel.TabIndex = 6;
+            this.txbTel.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // txbEmail
+            // 
+            this.txbEmail.BackColor = System.Drawing.SystemColors.Info;
+            this.txbEmail.Location = new System.Drawing.Point(187, 368);
+            this.txbEmail.Name = "txbEmail";
+            this.txbEmail.Size = new System.Drawing.Size(134, 20);
+            this.txbEmail.TabIndex = 7;
+            this.txbEmail.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // txbRg
+            // 
+            this.txbRg.BackColor = System.Drawing.SystemColors.Info;
+            this.txbRg.Location = new System.Drawing.Point(417, 368);
+            this.txbRg.Name = "txbRg";
+            this.txbRg.Size = new System.Drawing.Size(143, 20);
+            this.txbRg.TabIndex = 8;
+            // 
+            // txbEnd
+            // 
+            this.txbEnd.BackColor = System.Drawing.SystemColors.Info;
+            this.txbEnd.Location = new System.Drawing.Point(646, 368);
+            this.txbEnd.Name = "txbEnd";
+            this.txbEnd.Size = new System.Drawing.Size(133, 20);
+            this.txbEnd.TabIndex = 9;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.SystemColors.Info;
+            this.label4.Font = new System.Drawing.Font("Linux Libertine G", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(184, 334);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(147, 18);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "DIGITE SEU E-MAIL";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.SystemColors.Info;
+            this.label5.Font = new System.Drawing.Font("Linux Libertine G", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(432, 337);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(116, 18);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "DIGITE SEU RG";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.SystemColors.Info;
+            this.label6.Font = new System.Drawing.Font("Linux Libertine G", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(624, 334);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(175, 18);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "DIGITE SEU ENDEREÇO";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.Info;
+            this.button1.Font = new System.Drawing.Font("Linux Libertine G", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(704, 454);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(91, 23);
+            this.button1.TabIndex = 13;
+            this.button1.Text = "ACESSAR";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(905, 530);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txbEnd);
+            this.Controls.Add(this.txbRg);
+            this.Controls.Add(this.txbEmail);
+            this.Controls.Add(this.txbTel);
+            this.Controls.Add(this.txbCpf);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txbName);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txbName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txbCpf;
+        private System.Windows.Forms.TextBox txbTel;
+        private System.Windows.Forms.TextBox txbEmail;
+        private System.Windows.Forms.TextBox txbRg;
+        private System.Windows.Forms.TextBox txbEnd;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
